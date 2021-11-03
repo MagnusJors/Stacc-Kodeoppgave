97a7343e21ab4e64da6865876a32686ed7b11e52
MagnusJors <35961929+MagnusJors@users.noreply.github.com>
2021-11-03 22:53:39 +0100

    Fjerner tidligere søk, error, kommentarer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Stacc-Kodeoppgave
 Årets kodeoppgave er en åpen oppgave relatert til KYC. Det er opp til dere hvilken teknologi dere bruker til oppgaven og hvordan den løses. Det er dermed mulig å løse den uavhengig av om du foretrekker backend, frontend eller hele stacken. Det viktigste er at dere lager en applikasjon eller et API som gjør det mulig å utføre KYC sjekker av enkelt individer og/eller personer i et selskap.
+
+## Oppgavebeskrivelse
+> Har laget en enkel webapp som henter ut treff av personer på 'navn' i Stacc sitt api.
+> Printer navn og score gjennom søk som gir treff på 'navn' og error melding om ingen treff
+
+## Hvordan kjøre prosjektet
+> Prosjektet kjøres lokalt 
+> Åpnes i html, skriver inn navn i søkebaren og trykker på søk knappen. 
+
+## Kommentarer
+> 
--- backend.js
+++ backend.js
@@ -1,6 +1,9 @@
 function getScoreByName() {
+    // Name = tekst fra søkefelt 
     const name = document.getElementById("searchbar").value;
-    console.log(name)
+    clearHTML();
+
+    // GET PEP på navn
     fetch(`https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=${name}`)
         .then((response) => {
             if (response.ok) {
@@ -9,7 +12,10 @@ function getScoreByName() {
                 throw new Error("NETWORK RESPONSE ERROR");
             }
         })
+        // Legg til "score" og "name" i tabell
         .then(data => {
+            // Sjekker for navn i datasette
+
             const table = document.getElementById("table1")
             data.hits.forEach(element => {
                 const row = table.insertRow(1)
@@ -20,6 +26,25 @@ function getScoreByName() {
                 scoreCell.innerHTML = element.score
             });
         })
-        .catch((error) => console.error("FETCH ERROR:", error));
+        // Finner ikke navn, print error
+        .catch((error) => {
+            console.error("FETCH ERROR:", error);
+            document.getElementById("error-message").innerHTML = `ERROR: Could not find ${name}`
+        });
 
+}
+
+
+
+function clearHTML() {
+    // Fjern error beskjed
+    document.getElementById("error-message").innerHTML = ""
+
+    // Fjern innhold fra tabel
+    var tableHeaderRowCount = 1;
+    var table = document.getElementById('table1');
+    var rowCount = table.rows.length;
+    for (var i = tableHeaderRowCount; i < rowCount; i++) {
+        table.deleteRow(tableHeaderRowCount);
+    }
 }
